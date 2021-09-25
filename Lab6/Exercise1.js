46923cc0827a4b55ab9c21a4f01f82323ed7cdc3
lydiajun <89500597+lydiajun@users.noreply.github.com>
2021-09-25 01:48:23 -1000

    update

---
+++ Lab6/Exercise1.js
@@ -0,0 +1,8 @@
+var age =  20;
+var fav_num = 24;
+var day_of_birth = 24;
+var month_of_birth = 6;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);

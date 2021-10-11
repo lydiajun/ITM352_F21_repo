7a1cd7d6cd7fcc199ffac8385788fc05bd5efe58
lydiajun <89500597+lydiajun@users.noreply.github.com>
2021-10-11 02:15:58 -1000

    sp3

---
+++ SmartPhoneProducts3/products_data.js
@@ -0,0 +1,11 @@
+let product1 = {"name":"HTC", "price":40, "image":"http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"};
+
+let product2 = {"name":"Apple", "price":75, "image":"http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"};
+
+let product3 = {"name":"Nokia", "price":35, "image":"http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"};
+
+let product4 = {"name":"Samsung", "price":45, "image":"http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"};
+
+let product5 = {"name":"Blackberry", "price":10, "image":"http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"};
+
+var products = [product1, product2, product3, product4, product5];

1501e37c2313b2818e59ae97c4cdca75d9008e41
lydiajun <89500597+lydiajun@users.noreply.github.com>
2021-11-09 09:11:15 -1000

    lab12

---
+++ Jun_Lydia_Assignment1/products.js
@@ -0,0 +1,38 @@
+// Master products SNEAKERS
+[
+    // Product 1
+    {
+        "model":"Converse Chuck Taylor High Top",
+        "price":60,
+        "image":'converse.jpeg',
+        "quantity available":4
+    },
+    // Product 2
+    {
+        "model":"Nike Air Force 1 '07",
+        "price":90,
+        "image":'af1.jpeg',
+        "quantity available":2
+    },
+    // Product 3
+    {
+        "model":"Vans Sk8 Hi",
+        "price":70,
+        "image":'vans.jpg',
+        "quantity available":5
+    },
+    // Product 4
+    {
+        "model":"Reebok Club C 85",
+        "price":70,
+        "image":'reebok.jpg',
+        "quantity available":3
+    },
+    // Product 5
+    {
+        "model":"Nike Blazer Mid '77",
+        "price":100,
+        "image":'blazer.jpeg',
+        "quantity available":4
+    }
+]

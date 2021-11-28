dfc813a2ab2ed02518078540b91bf66b8a2d5902
lydiajun <89500597+lydiajun@users.noreply.github.com>
2021-11-28 01:36:26 -1000

    assignment 2

--- Jun_Lydia_Assignment2/server.js
+++ Jun_Lydia_Assignment2/server.js
@@ -8,6 +8,13 @@ var products = data.products;
 products.forEach((prod, i) => { prod.inventory = 10; });
 var querystring = require("querystring");
 
+var users_reg_data = 
+{
+    "dport": {"password":"portpass"},
+    "kazman": {"password":"kazpass"},
+    "itm352": {"password":"grader"}
+};
+
 // Routing 
 
 // Monitor all requests
@@ -70,13 +77,41 @@ app.post("/process_form", function (req, res, next) {
         for (i = 0; i < products.length; i++) {
             products[i].inventory -= Number(POST['quantity' + i]); 
         }
-        // Redirect customer to invoice.html if valid quantities entered.
-        res.redirect("./invoice.html?" + QString);
+        // Redirect customer to signin.html if valid quantities entered.
+        res.redirect("./signin.html?" + QString);
     } else { // Else, give the errors.
         let errObj = { 'error': JSON.stringify(errors) };
         QString += '&' + querystring.stringify(errObj);
         res.redirect("./store.html?" + QString); // Redirect to store.html if errors.
     }
+});
+
+// POST request to signin.html. Taken from Assignment 2 code examples on ITM 352 website.
+app.post("/signin.html", function (request, response) {
+    let params = new URLSearchParams(request.query);
+    // Process login form POST and redirect to logged in page if ok, back to login page if not.
+    the_username = request.body['username'].toLowerCase();
+    the_password = request.body['password'];
+    if (typeof users_reg_data[the_username] != 'undefined') { 
+        if (users_reg_data[the_username].password == the_password) { // Check if password matches username.
+            response.redirect('./invoice.html?'+ params.toString()); // Send to invoice page if login successful.
+        } else {
+            response.send(`Wrong password!`); // Error message for wrong password.
+        }
+        return;
+    }
+    response.send(`${the_username} does not exist`); // Error message for user that doesn't exist.
+});
+
+// POST request to signup.html.
+app.post("/signup.html", function (request, response) {
+    // Registration validation
+        // Check if email address is valid. Case insensitive.
+        // Name should only allow letters. 30 characters or less.
+        // Check if username is unique. Letters and numbers only. 4-10 characters. Case insensitive.
+        // Password is 6 characters min. Case sensitive.
+        // Check if "Confirm Password" is same as password.
+    // Send to invoice page if registration successful.
 });
 
 // Route all other GET requests to files in public.
